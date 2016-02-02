0e220c410234cadbd38822b577708b53126de36f
bbastou <stoun77@gmail.com>
2016-02-02 22:01:27 +0100

    Clean up colos.js
    Delete old function hsbToRgb

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -5,50 +5,6 @@
  * More info : https://github.com/bjohnso5/hue-hacking 
  * Copyright (c) 2013 Bryan Johnson; Licensed MIT */
 
-var hsbToRgb = function(hue, sat, value) {
-var satNormal = sat / 255,
-        valueNormal = value / 255,
-        hueNormal = (hue / 65535) * 360,
-        c = valueNormal * satNormal,
-        x = c * (1 - Math.abs(((hueNormal / 60) % 2) - 1)),
-        m = valueNormal - c,
-        red = 0,
-        green = 0,
-        blue = 0;
-if ((hueNormal >= 0) && (hueNormal < 60)) {
-        red = c;
-        green = x;
-        blue = 0;
-    } else if ((hueNormal >= 60) && (hueNormal < 120)) {
-        red = x;
-        green = c;
-        blue = 0;
-    } else if ((hueNormal >= 120) && (hueNormal < 180)) {
-        red = 0;
-        green = c;
-        blue = x;
-    } else if ((hueNormal >= 180) && (hueNormal < 240)) {
-        red = 0;
-        green = x;
-        blue = c;
-    } else if ((hueNormal >= 240) && (hueNormal < 300)) {
-        red = x;
-        green = 0;
-        blue = c;
-    } else {
-        red = c;
-        green = 0;
-        blue = x;
-    }
-    return [colorNormalizedToEightBit(red + m), colorNormalizedToEightBit(green + m), colorNormalizedToEightBit(blue + m)];
-    
-};
-
-var colorNormalizedToEightBit = function(value) {
-    return (Math.round(value * 255));
-};
-
-
 var XYPoint = function (x, y) {
     this.x = x;
     this.y = y;
